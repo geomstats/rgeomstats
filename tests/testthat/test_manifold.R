c5f3e9f472773291ceff3f1946027d335131dbaf
lundjack <35204227+lundjack@users.noreply.github.com>
2018-10-10 11:37:01 -0700

    update test_manifold

--- tests/testthat/test_manifold.R
+++ tests/testthat/test_manifold.R
@@ -1,4 +1,19 @@
 # Unit tests for Manifold.
 
+library(reticulate)
+gs <- import_from_path("geomstats", path = ".")
+
+
 manifold <- Manifold$new(dimension = 3)
 manifold$dimension == 3
+
+SetUp <- function(){
+  rand <<- ceiling(10*runif(1))
+  manifold <<- Manifold$new(dimension = rand)
+}
+
+TestDimension <- function(){
+  result <<- manifold$dimension
+  expected <<- rand
+  stopifnot(result == expected)
+}
