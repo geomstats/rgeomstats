deaa1d59940cfe1071a5020894ef9c0d6b57c6e2
lundjack <35204227+lundjack@users.noreply.github.com>
2018-10-18 14:31:58 -0700

    unit tests manifold and euclidean space using test_that

--- tests/testthat/test_manifold.R
+++ tests/testthat/test_manifold.R
@@ -1,26 +1,8 @@
-# Unit tests for Manifold.
 
-library(reticulate)
-gs <- import_from_path("geomstats", path = ".")
-
-  rand <<- ceiling(10*runif(1))
-  manifold <<- Manifold$new(dimension = rand)
-
-
-
-test_that("testing dimension",{
-  TestDimension <- function(){
-    result <<- manifold$dimension
-    expected <<- rand
-    test.dimension = result == expected
-    return(test.dimension)
-  }
-  expect_true(TestDimension())
+test_that("Unit tests for Manifold",{
+  rand <- ceiling(10*runif(1))
+  manifold <- Manifold$new(dimension = rand)
+  result <- manifold$dimension
+  expected <- rand
+  expect_equal(result, expected)
 })
-
-expect_true()
-
-TestBelongs <- function(){
-  point <- euclidean.space$RandomUniform()
-  belongs <- euclidean.space$Belongs(point)
-}
