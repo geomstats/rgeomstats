371d49168fe361c0596261dfae206c2d3e82b681
lundjack <35204227+lundjack@users.noreply.github.com>
2018-10-18 16:18:24 -0700

    vectorization spacing

--- rgeomstats/vectorization.R
+++ rgeomstats/vectorization.R
@@ -3,13 +3,13 @@ ToNdarray <- function(x, to.ndim, axis = 0){
   if (CurrentNdim == to.ndim - 1) {
     if (axis == 0) {
       NewDim <- c(1, dim(x))
-      NewX <- array(x,dim = NewDim)
+      NewX <- array(x, dim = NewDim)
     } else if (axis == CurrentNdim) {
       NewDim <- c(dim(x), 1)
-      NewX <- array(x,dim = NewDim)
+      NewX <- array(x, dim = NewDim)
     } else {
       NewDim <- c(dim(x)[1:axis], 1, dim(x)[(axis + 1):length(dim(x))])
-      NewX <- array(x,dim = NewDim)
+      NewX <- array(x, dim = NewDim)
     }
   }
   return(NewX)
