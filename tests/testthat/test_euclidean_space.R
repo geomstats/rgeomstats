8bebb15f6bea21069510eeec825d75c872701bc8
lundjack <35204227+lundjack@users.noreply.github.com>
2018-10-18 14:31:58 -0700

    unit tests manifold and euclidean space using test_that

--- tests/testthat/test_euclidean_space.R
+++ tests/testthat/test_euclidean_space.R
@@ -1,19 +1,11 @@
-# Unit tests for Euclidean Space
-
-setUp <- function(){
+test_that("Unit tests for Euclidean Space", {
   rand <<- ceiling(10*runif(1))
   euclidean.space <<- EuclideanSpace$new(dimension = rand)
-}
-
-test_that("instantiating random dimension 1-10", {
-  rand <<- ceiling(10*runif(1))
-  euclidean.space <<- EuclideanSpace$new(dimension = rand)
-
   expect_equal(euclidean.space$dimension,rand)
 })
 
 
-test_that("RandomUniform and Belongs together", {
+test_that("Unit tests for Euclidean Space", {
   point <- array(euclidean.space$RandomUniform())
   belongs <- euclidean.space$Belongs(point)
   expect_true(belongs)
