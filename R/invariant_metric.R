3a516bea165f93981f6341ff7da7c2526e2c1cf7
Nina Miolane <ninamio78@gmail.com>
2019-08-08 14:47:21 -0700

    Nina clean up try

--- R/invariant_metric.R
+++ R/invariant_metric.R
@@ -7,21 +7,20 @@ InvariantMetric <- setRefClass("InvariantMetric",
     initialize = function(group, inner.product.mat.at.identity = NULL,
                           left.or.right = "left") {
       if (is.null(inner.product.mat.at.identity)) {
-        inner.product.mat.at.identity <- diag(1, group$dimension)
+        .self$inner.product.mat.at.identity <- diag(1, group$dimension)
+      } else {
+        .self$inner.product.mat.at.identity <- inner.product.mat.at.identity
       }
-      mat.shape <- dim(inner.product.mat.at.identity)
+
+      mat.shape <- dim(.self$inner.product.mat.at.identity)
       stopifnot(left.or.right == "left" || "right")
-      eigenvalues <- eigen(inner.product.mat.at.identity)
+      eigenvalues <- eigen(.self$inner.product.mat.at.identity)
       eigenvalues <- eigenvalues$values
       n.pos.eigval <- sum(eigenvalues > 0)
       n.neg.eigval <- sum(eigenvalues < 0)
       n.null.eigval <- sum(eigenvalues < .Machine$double.eps ^ 0.5)
       .self$group <- group
 
-      if (is.null(inner.product.mat.at.identity)) {
-        inner.product.mat.at.identity <- diag(1, .self$group$dimension)
-      }
-      .self$inner.product.mat.at.identity <- inner.product.mat.at.identity
       .self$left.or.right <- left.or.right
     },
 
